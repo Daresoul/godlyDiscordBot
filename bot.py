389de6876c930a662092adae997a70c3b66350f1
Nicolas <nicolas.dyhrman@gmail.com>
2022-08-04 02:32:31 +0200

    added error handling

--- bot.py
+++ bot.py
@@ -15,4 +15,16 @@ async def change_name(ctx, member : discord.Member, arg):
     await member.edit(nick=arg)
     await ctx.send("Name has been changed to: " + arg)
 
+@change_name.error
+async def change_name_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('I could not find that member...')
+    if isinstance(error, commands.BotMissingPermissions):
+        await ctx.send('Luls, can\'t change the members nickname...')
+    if isinstance(error, commands.MemberNotFound):
+        await ctx.send('Cannot find the Member you are speaking of...')
+    if isinstance(error, commands.TooManyArguments):
+        await ctx.send('Ummm, have u looked in the help command?')
+
+
 discord_bot.run(settings.TOKEN)
