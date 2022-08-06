93b1d5b0df4c853784fb7feb54641dfbe5d9a7e2
Nicolas <nicolas.dyhrman@gmail.com>
2022-08-07 00:30:33 +0200

    bot changes

--- OnJoinBot.py
+++ OnJoinBot.py
@@ -257,9 +257,9 @@ def printY(arr, wordEmoji, fillEmoji):
 def printZ(arr, wordEmoji, fillEmoji):
     arr[0] += fillEmoji + fillEmoji + fillEmoji
     arr[1] += wordEmoji + wordEmoji + wordEmoji
-    arr[2] += wordEmoji + fillEmoji + fillEmoji
+    arr[2] += fillEmoji + fillEmoji + wordEmoji
     arr[3] += fillEmoji + wordEmoji + fillEmoji
-    arr[4] += fillEmoji + fillEmoji + wordEmoji
+    arr[4] += wordEmoji + fillEmoji + fillEmoji
     arr[5] += wordEmoji + wordEmoji + wordEmoji
     arr[6] += fillEmoji + fillEmoji + fillEmoji
 
